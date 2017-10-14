46ddb91bdacdb94c4c1e811e7b91ddcf2ad4068d
Tomohiro Matsuo <gacktomo@gmail.com>
2017-10-15 01:44:16 +0900

    Fix 075C

--- 075/ABC075-C.rb
+++ 075/ABC075-C.rb
@@ -1,38 +1,36 @@
 s = gets.chomp.split(" ")
-n = s[0].to_i
-m = s[1].to_i
-list = Array.new(n).map{Array.new()}
+$n = s[0].to_i
+$m = s[1].to_i
+$list = Array.new($n).map{Array.new($n,false)}
+$visited = Array.new($n,false)
 
-for i in 0..m-1 do
+for i in 0..$m-1 do
     s = gets.chomp.split(" ")
-    a = s[0].to_i
-    b = s[1].to_i
-    list[a-1].push(b)
-    list[b-1].push(a)
+    a = s[0].to_i-1 
+    b = s[1].to_i-1
+    $list[a][b] = $list[b][a] = true
 end
 
-def check(i, j, n, m, list)
-    clist = []
-    for i2 in 0..n-1 do
-        for j2 in 0..list[i].size-1 do
-            if i!=i2 and j!=j2 then
-                clist[list[i2][j2].to_i] = true
-            end
-        end
+def check( i=0 )
+    $visited[i] = true
+    for j in 0..$n-1 do
+        check(j) if $list[i][j] and $visited[j] == false
     end
 
-    if clist.count {|item| item == nil } >= 1 then
-        return false
-    else return true 
-    end
+    if $visited.count {|num| num == false } >= 1 then return false
+    else return true end
 end
 
 count = 0
-for i in 0..n-1 do
-    for j in 0..list[i].size-1 do
-        count += 1 if check( i, j,n,m, list) == false
+for i in 0..$n-1 do
+    for j in 0..i do
+        if $list[i][j] then
+            $list[i][j] = $list[j][i] = false
+            $visited.map!{false}
+            count += 1 if check() == false
+            $list[i][j] = $list[j][i] = true
+        end
     end
 end
 
 puts count
-
